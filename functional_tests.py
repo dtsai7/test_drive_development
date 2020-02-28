98009b9aa5b581a4b3253232f6b745d55d52f089
Chi Shiun Tsai <david.tsai0811@gmail.com>
2020-02-28 11:43:38 +0800

    fix typo and add waiting time

--- functional_tests.py
+++ functional_tests.py
@@ -7,6 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
     
     def tearDown(self):
         self.browser.quit()
